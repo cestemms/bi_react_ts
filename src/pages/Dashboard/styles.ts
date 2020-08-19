ac6bc7a3d26fc702b04933ffb7884b46f1ee183f
Kayne G.I <kayne.goncalves@healthbit.com.br>
2020-08-18 21:44:46 -0300

    pages folder

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 1.5rem;
+  margin-top: 2rem;
+  margin-left: 1rem;  
+  margin-right: 1rem; 
+  
+  border-radius: 1rem;
+  height: calc(100vh - (var(--nav-size)*1.5));
+  
+  background: var(--card-background);
+`;
+
+
+
+
+
+
+
+
---
+++ src/pages/Page/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+
+const Page: React.FC = () => {
+
+  return (
+
+    <Container>
+      Página Teste
+    </Container>
+
+    );
+}
+
+export default Page;
